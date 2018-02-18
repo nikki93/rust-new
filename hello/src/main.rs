da3da1758a759e2cf06424fce179874fb0f82ad5
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-02-17 16:40:59 -0800

    [hello] Add and try `gcd(...)` function

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,18 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
 fn main() {
-    println!("hai perry! :o");
+    let m = 25;
+    let n = 60;
+    println!("gcd of {} and {} is: {}", m, n, gcd(m, n));
 }
