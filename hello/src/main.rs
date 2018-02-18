526dae511116367b576f81e0b0276910e9c7575e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-02-17 17:25:25 -0800

    [hello] Add test for `gcd(...)`

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -11,6 +11,14 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(13, 13), 13);
+    assert_eq!(gcd(37, 600), 3);
+    assert_eq!(gcd(20, 100), 20);
+    assert_eq!(gcd(624129, 2061517), 18913);
+}
+
 fn main() {
     let m = 25;
     let n = 60;
