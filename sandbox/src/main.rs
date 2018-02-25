a7233ae1b1c0ef6a842d7d11eca801fd71c17cb4
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-02-25 01:31:58 -0800

    [sandbox] Experiment with borrow stuff, try 'rust-lldb'

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,10 @@
+fn foo() -> i32 {
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    z
+}
+
 fn main() {
     const MAX: usize = 3000000;
     let mut sieve = [true; MAX];
@@ -12,4 +19,19 @@ fn main() {
     }
     let n = 2000107;
     println!("{} is prime? {}", n, sieve[n]);
+
+    let mut s = "hello, world".to_owned();
+    {
+        let k = &mut s[3..6];
+        k.make_ascii_uppercase();
+    }
+    println!("{}", s);
+
+    let v = vec!["hello,", "world!", "yay"];
+    println!("{}", v.join(" "));
+
+    println!("{}", "th\u{e9}");
+    println!("{}", "the\u{301}");
+
+    println!("{}", foo());
 }
